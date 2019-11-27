fc4c384c66e78500169221cd57f740591842f96a
Egorychev Artem <temych_2000@mail.ru>
2019-11-28 01:20:19 +0300

    Scripted auto-commit on change (2019-11-28 01:20:19) by gitwatch.sh

--- lab5/src/main/java/App.java
+++ lab5/src/main/java/App.java
@@ -1,2 +1,35 @@
+import akka.NotUsed;
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.http.javadsl.ConnectHttp;
+import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
+import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
+
+import java.util.concurrent.CompletionStage;
+
 public class App {
+    ActorSystem system = ActorSystem.create("routes");
+    final Http http = Http.get(system);
+    final ActorMaterializer materializer = ActorMaterializer.create(system);
+
+    App app = new App();
+    ActorRef router = system.actorOf(Props.create(ActorRouter.class, 5));
+
+    Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = app.createRoute(system, router).flow(system, materializer);
+    CompletionStage<ServerBinding> binding = http.bindAndHandle(
+            routeFlow,
+            ConnectHttp.toHost(HOST, PORT),
+            materializer
+    );
+        System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
+        System.in.read();
+        binding
+                .thenCompose(ServerBinding::unbind)
+            .thenAccept(unbound -> system.terminate());
+
 }
