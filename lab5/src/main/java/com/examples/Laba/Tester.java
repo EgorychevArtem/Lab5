2873f39027eacef75706d037c61e497b9ed146be
Egorychev Artem <temych_2000@mail.ru>
2019-11-28 23:01:12 +0300

    Scripted auto-commit on change (2019-11-28 23:01:12) by gitwatch.sh

--- lab5/src/main/java/com/examples/Laba/Tester.java
+++ lab5/src/main/java/com/examples/Laba/Tester.java
@@ -1,10 +1,13 @@
 package com.examples.Laba;
 
+import akka.actor.ActorRef;
 import akka.stream.ActorMaterializer;
 import org.asynchttpclient.AsyncHttpClient;
 
 public class Tester {
     AsyncHttpClient asyncHttpClient;
     ActorMaterializer materializer;
-    
+    ActorRef storage;
+
+    public Tester()
 }
