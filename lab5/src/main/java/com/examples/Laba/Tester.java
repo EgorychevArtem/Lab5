f19991d8aa4279e4bc2bc9389244e08b31c2f291
Egorychev Artem <temych_2000@mail.ru>
2019-11-29 00:47:18 +0300

    Scripted auto-commit on change (2019-11-29 00:47:18) by gitwatch.sh

--- lab5/src/main/java/com/examples/Laba/Tester.java
+++ lab5/src/main/java/com/examples/Laba/Tester.java
@@ -1,9 +1,13 @@
 package com.examples.Laba;
 
+import akka.NotUsed;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
 import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
 import org.asynchttpclient.AsyncHttpClient;
 
 public class Tester {
@@ -15,5 +19,9 @@ public class Tester {
         this.asyncHttpClient = asyncHttpClient;
         this.materializer = materializer;
         this.storage = system.actorOf(Props.create(TestStorage.class));
+    }
+
+    public Flow<HttpRequest, HttpResponse, NotUsed> createRoute(){
+
     }
 }
