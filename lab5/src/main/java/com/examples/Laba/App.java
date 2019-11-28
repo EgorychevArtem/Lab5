8e24a614f6c5c1f13c8e1602a3b61e4682f1f1e0
Egorychev Artem <temych_2000@mail.ru>
2019-11-29 00:47:26 +0300

    Scripted auto-commit on change (2019-11-29 00:47:26) by gitwatch.sh

--- lab5/src/main/java/com/examples/Laba/App.java
+++ lab5/src/main/java/com/examples/Laba/App.java
@@ -27,7 +27,7 @@ public class App {
 
         AsyncHttpClient httpClient = asyncHttpClient();
         Tester tester = new Tester(asyncHttpClient(), system, materializer);
-        Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = tester.
+        Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = tester.createRoute();
         CompletionStage<ServerBinding> binding = http.bindAndHandle(
                 routeFlow,
                 ConnectHttp.toHost(HOST, PORT),
