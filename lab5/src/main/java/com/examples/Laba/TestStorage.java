a742f67b592e866c1604eacc86aee522ba0db5ee
Egorychev Artem <temych_2000@mail.ru>
2019-11-28 23:04:54 +0300

    Scripted auto-commit on change (2019-11-28 23:04:54) by gitwatch.sh

--- lab5/src/main/java/com/examples/Laba/TestStorage.java
+++ lab5/src/main/java/com/examples/Laba/TestStorage.java
@@ -1,6 +1,10 @@
 package com.examples.Laba;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class TestStorage {
-    
+    Map<Long, ArrayList<Test>> storage = new HashMap<>();
 
 }
