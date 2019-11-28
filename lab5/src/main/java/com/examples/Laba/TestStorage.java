649bf4ffc66c306c54654f2fd8079ce8452a6a35
Egorychev Artem <temych_2000@mail.ru>
2019-11-28 23:06:27 +0300

    Scripted auto-commit on change (2019-11-28 23:06:27) by gitwatch.sh

--- lab5/src/main/java/com/examples/Laba/TestStorage.java
+++ lab5/src/main/java/com/examples/Laba/TestStorage.java
@@ -1,10 +1,19 @@
 package com.examples.Laba;
 
+import akka.actor.AbstractActor;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-public class TestStorage {
+public class TestStorage extends AbstractActor {
     Map<Url, Long> storage = new HashMap<Url, Long>();
 
+    @Override
+    public Receive createReceive() {
+        return null;
+    }
+
+    @Override
+    public Receive
 }
