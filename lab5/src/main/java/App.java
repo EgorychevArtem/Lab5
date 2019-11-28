a613f3db22fc17dce8c58e43068715e6353060dd
Egorychev Artem <temych_2000@mail.ru>
2019-11-28 22:55:51 +0300

    Scripted auto-commit on change (2019-11-28 22:55:51) by gitwatch.sh

--- lab5/src/main/java/App.java
+++ lab5/src/main/java/App.java
@@ -11,13 +11,14 @@ import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
 import org.asynchttpclient.AsyncHttpClient;
 
+import java.io.IOException;
 import java.util.concurrent.CompletionStage;
 import static org.asynchttpclient.Dsl.asyncHttpClient;
 
 public class App {
     private static String HOST = "localhost";
     private static int PORT = 8080;
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         ActorSystem system = ActorSystem.create("routes");
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
