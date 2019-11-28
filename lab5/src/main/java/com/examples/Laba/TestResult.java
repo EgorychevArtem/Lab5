25e208c510557289ef288cdc7f44a0f9316d6410
Egorychev Artem <temych_2000@mail.ru>
2019-11-29 00:26:44 +0300

    Scripted auto-commit on change (2019-11-29 00:26:44) by gitwatch.sh

--- lab5/src/main/java/com/examples/Laba/TestResult.java
+++ lab5/src/main/java/com/examples/Laba/TestResult.java
@@ -1,5 +1,11 @@
 package com.examples.Laba;
 
 public class TestResult {
-    
+    Url test;
+    Long res;
+
+    TestResult(Url test, Long res){
+        this.res = res;
+        this.test = test;
+    }
 }
