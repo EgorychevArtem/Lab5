381d88fc3f27585a7ab67674c3af0c144ee3fa57
Egorychev Artem <temych_2000@mail.ru>
2019-12-06 01:19:51 +0300

    Scripted auto-commit on change (2019-12-06 01:19:51) by gitwatch.sh

--- lab5/src/main/java/com/examples/Laba/Tester.java
+++ lab5/src/main/java/com/examples/Laba/Tester.java
@@ -69,8 +69,7 @@ public class Tester {
                     if(res.isPresent()){
                         return CompletableFuture.completedFuture(res.get());
                     } else{
-                       // return RunTest(test);
-                        
+                        return RunTest(test);
                     }
                 });
     }
